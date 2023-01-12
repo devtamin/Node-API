9a8ea9a4f599bae7d7dd1579ae5fd2b9d1916cdf
Alex <phithounsavanh@gmail.com>
2023-01-12 14:50:44 -0500

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 
@@ -12,6 +13,15 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog, My name is Devtamin')
 })
 
-app.listen(3000, ()=> {
-    console.log(`Node API app is running on port 3000`)
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect('mongodb+srv://admin:12345678Admin@devtaminapi.zpncstm.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log(`Node API app is running on port 3000`)
+    });
+}).catch((error) => {
+    console.log(error)
 })
