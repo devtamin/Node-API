06244e9977083132f7e5b365c0392618342995bb
Alex <phithounsavanh@gmail.com>
2023-01-12 12:30:03 -0500

    Install Nodemon

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send('Hello NODE API')
 })
 
+app.get('/blog', (req, res) => {
+    res.send('Hello Blog, My name is Devtamin')
+})
+
 app.listen(3000, ()=> {
     console.log(`Node API app is running on port 3000`)
 })
