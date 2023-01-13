ca5e0075141cac93b8f4ce9411d70880215417fd
Alex <phithounsavanh@gmail.com>
2023-01-12 20:40:29 -0500

    create a route for saving a product to the database

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 
+app.use(express.json())
 
 //routes
 
@@ -13,6 +15,17 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog, My name is Devtamin')
 })
 
+
+app.post('/product', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
 
 mongoose.set("strictQuery", false)
 mongoose.
