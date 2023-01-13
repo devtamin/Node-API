7ec5866f0a7a13d68a510629a5292e4ed3b15644
Alex <phithounsavanh@gmail.com>
2023-01-13 11:37:44 -0500

    fetch product data

--- server.js
+++ server.js
@@ -15,8 +15,27 @@ app.get('/blog', (req, res) => {
     res.send('Hello Blog, My name is Devtamin')
 })
 
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
-app.post('/product', async(req, res) => {
+app.get('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+app.post('/products', async(req, res) => {
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product);
