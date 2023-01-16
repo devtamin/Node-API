86dad080e8476cc1a9bff4dc969a53a1617cad7e
Alex <phithounsavanh@gmail.com>
2023-01-16 18:54:12 -0500

    delete

--- server.js
+++ server.js
@@ -64,6 +64,22 @@ app.put('/products/:id', async(req, res) => {
     }
 })
 
+// delete a product
+
+app.delete('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 mongoose.set("strictQuery", false)
 mongoose.
 connect('mongodb+srv://admin:12345678Admin@devtaminapi.zpncstm.mongodb.net/Node-API?retryWrites=true&w=majority')
